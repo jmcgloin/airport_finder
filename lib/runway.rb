2fca4c4573c158467470f7339d971f57c3a9f154
Jason McGloin <jason.mcgloin@gmail.com>
2019-07-05 12:58:57 -0400

    created find and create methods for runway

--- lib/runway.rb
+++ lib/runway.rb
@@ -1,13 +1,24 @@
 class Runway
 
+	@@all = []
+
 	def initialize(name, length, width, airport)
 		@name = name
 		@length = length
 		@width = width
+		@airport = airport
 	end
 
 	def self.create(name, length, width, airport)
 		#take in the runway table and parse the info for display
+	end
+
+	def self.find(name, length, width, airport)
+		self.all.detect{ |rway| rway.name == name && rway.airport == airport }
+	end
+
+	def self.find_or_create(name, length, width, airport)
+		self.find(name, length, width, airport) || self.create(name, length, width, airport)
 	end
 
 end
