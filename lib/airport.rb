dbc2e73926ca8d3c231305ed7147ce4c5576fa57
Jason McGloin <jason.mcgloin@gmail.com>
2019-07-11 01:06:01 -0400

    refactoring, cleaned up airport class, created #create_runway and #create_details_keys

--- lib/airport.rb
+++ lib/airport.rb
@@ -28,71 +28,77 @@ class Airport
 		Airport.new(identifier, name).tap{ |airport| airport.make_details_hash(details_array) }
 	end
 
-	def make_details_hash(details_array)
+	def create_runway(runway_names, runways_table)
+		Runway.create_runway_from_data(runway_names, runways_table, self)
+	end
 
+	def create_details_keys(headings)
+		headings.children.each do |child|
+			if !(['Instrument Procedures', 'Runway Information'].include?(child.text) || child.text.include?("Other Pages"))
+				self.details[child.text.to_sym] = {}
+			end
+		end
+	end
+
+	def make_details_hash(details_array)
+################### HOW TO REFACTOR THIS??????? ############################
+# one method that does the logical checking and calls other method to do the iterating/assigning/returning
 		headings = details_array[0]
-		runway_names = details_array[1]
 		content_table = details_array[2]
-		runways_table = details_array[3]
 		more_info =  details_array[4]
 
-		Runway.create_runway_from_data(runway_names, runways_table, self)
+		create_runway(details_array[1], details_array[3])
 
 		section_heading, section_content = "", []
 
-		headings.children.each{ |child| self.details[child.text.to_sym] = {} }
-		self.details.delete(:"Instrument Procedures")
-		self.details.delete(:"Runway Information")
+		create_details_keys(headings)
 
 		j = 0
-		for i in (0..details.keys.count - 1) do 
-			if details.keys[i].to_s.include? "Other Pages"
-				self.details.delete(details.keys[i])
-			else
-				filtered_content = content_table[j].children.select{ |child| child.text.strip != "" }
-				filtered_content.each.with_index do |subsection, k|
-					if i != 3 && i != 5
-						section_heading = subsection.children[0].text
-						section_content = subsection.children[1].children.map do |el|
-							el.text.gsub(/\u00a0/, '')
-						end.select{ |et| et.strip != "" }
-						# binding.pry
-					elsif i == 3
-						if k == 0
-							sec_split = subsection.text.split(/\u00a0{2}/)
-							section_heading = "   "
-							section_content = "#{sec_split[0]} |#{sec_split[1]}               |#{sec_split[2]}   |#{sec_split[3]}"
-						else
-							sec_split = subsection.children.map do |el|
-								el.text.gsub(/\u00a0/, '').strip
-							end.select{ |el| el != "" }
-							section_heading = k.to_s + ":" + (" " * (3 - k.to_s.chars.count))
-							section_content = "#{sec_split[0]}#{" " * (20 - sec_split[0].chars.count)}|#{sec_split[1]}#{" " * (23 - sec_split[1].chars.count)}|#{sec_split[2]}|#{sec_split[3]}"
-						end
-					elsif i == 5
-						trs = subsection.css("tr")
-						section_heading = []
-						section_content = []
-						trs.each do |tr|
-							section_heading << tr.children[0].text if tr.children[0]
-							if tr.children[2]
-								section_content << tr.children[2].text
-							elsif tr.children[1]
-								section_content << tr.children[1].text
-							else
-								section_content << ""
-							end
-						end
-					end
-					if i != 5
-						details[details.keys[i]][section_heading] = section_content
+		for i in (0..self.details.keys.count - 1) do 
+			
+			filtered_content = content_table[j].children.select{ |child| child.text.strip != "" }
+			filtered_content.each.with_index do |subsection, k|
+				if i != 3 && i != 5
+					section_heading = subsection.children[0].text
+					section_content = subsection.children[1].children.map do |el|
+						el.text.gsub(/\u00a0/, '')
+					end.select{ |et| et.strip != "" }
+				elsif i == 3
+					if k == 0
+						sec_split = subsection.text.split(/\u00a0{2}/)
+						section_heading = "   "
+						section_content = "#{sec_split[0]} |#{sec_split[1]}               |#{sec_split[2]}   |#{sec_split[3]}"
 					else
-						section_heading.each.with_index{ |sh, m| details[details.keys[i]][sh] = section_content[m]}
+						sec_split = subsection.children.map do |el|
+							el.text.gsub(/\u00a0/, '').strip
+						end.select{ |el| el != "" }
+						section_heading = k.to_s + ":" + (" " * (3 - k.to_s.chars.count))
+						section_content = "#{sec_split[0]}#{" " * (20 - sec_split[0].chars.count)}|#{sec_split[1]}#{" " * (23 - sec_split[1].chars.count)}|#{sec_split[2]}|#{sec_split[3]}"
 					end
-
+				elsif i == 5
+					trs = subsection.css("tr")
+					section_heading = []
+					section_content = []
+					trs.each do |tr|
+						section_heading << tr.children[0].text if tr.children[0]
+						if tr.children[2]
+							section_content << tr.children[2].text
+						elsif tr.children[1]
+							section_content << tr.children[1].text
+						else
+							section_content << ""
+						end
+					end
 				end
-				j += 1
+				if i != 5
+					details[details.keys[i]][section_heading] = section_content
+				else
+					section_heading.each.with_index{ |sh, m| details[details.keys[i]][sh] = section_content[m]}
+				end
+
 			end
+			j += 1
+			
 		end
 	end
 end
