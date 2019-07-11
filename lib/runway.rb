61c219979aaffa194f5bd8cef415d5ec40b3a428
Jason McGloin <jason.mcgloin@gmail.com>
2019-07-11 00:29:58 -0400

    refactoring, cleaned up runway class

--- lib/runway.rb
+++ lib/runway.rb
@@ -4,43 +4,41 @@ class Runway
 
 	attr_accessor :name, :dimensions, :airport, :surface
 
-	def initialize(name, dimensions, airport)
+	def initialize(name, airport)
 		@name = name
-		@dimensions = dimensions
 		@airport = airport
 	end
 
-	def self.create(name, dimensions, airport)
-		self.new(name, dimensions, airport)
+	def self.create(name, airport)
+		self.new(name, airport).tap{ |rw| self.all << rw }
 	end
 
 	def self.find(name, airport)
 		self.all.detect{ |rway| rway.name == name && rway.airport == airport }
 	end
 
-	def self.find_or_create(name, dimensions, airport)
-		self.find(name, airport) || self.create(name, dimensions, airport)
+	def self.find_or_create(name, airport)
+		self.find(name, airport) || self.create(name, airport)
 	end
 
 	def self.create_runway_from_data(runway_names, runway_data, airport)
-		############## fix this:  use a hash instead of two arrays ###############
-		runway_dimensions = []
-		runway_surfaces = []
-		runway_data.children.each do |child|
+
+		runway_counter = 0
+		runway_data.children.each.with_index do |child, i|
 			if child.children.count > 0
 				case child.children[0].text.gsub(/\u00a0/, '').strip
 				when "Dimensions:"
-					runway_dimensions << child.children[1].text.gsub(/\u00a0/, '').strip
+					rw = self.find_or_create(runway_names[runway_counter].text, airport)
+					rw.dimensions = child.children[1].text.gsub(/\u00a0/, '').strip
 				when "Surface:"
-					runway_surfaces << child.children[1].text.gsub(/\u00a0/, '').strip
+					rw = self.find_or_create(runway_names[runway_counter].text, airport)
+					rw.surface = child.children[1].text.gsub(/\u00a0/, '').strip
+					rw.add_to_airport(airport)
+					runway_counter += 1
 				end
 			end
 		end
-		runway_names.each.with_index do |name, i|
-			rw = self.find_or_create(name.text, runway_dimensions[i], airport)
-			rw.surface = runway_surfaces[i]
-			rw.add_to_airport(airport)
-		end
+
 	end
 
 	def add_to_airport(airport)
